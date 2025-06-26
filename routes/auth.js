3650fb7531cc16e05f25d22466358325f2537a99
pushplata-sahu <sahupushplata941@gmail.com>
2025-06-26 20:22:50 +0530

    🐛 Fix: case-sensitive import for user model

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 router.post("/signup", async (req, res) => {
